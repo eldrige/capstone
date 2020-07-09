cbbebf456c0d56b3ec69be40ce7481319890a56c
Apoh Eldrige <peldrige8@gmail.com>
2020-07-09 22:17:43 +0100

    modified delete functionality

--- script.js
+++ script.js
@@ -1,53 +1,31 @@
+// node elements
+const xterAmtRange = document.getElementById("range");
+const xterAmtNumber = document.getElementById("number");
+const generateBtn = document.getElementById("generate");
+const form = document.getElementById("form");
+const displayPassword = document.querySelector(".password-display");
+const asciiXters = arrayFromLowToHigh(97, 122)
+  .concat(arrayFromLowToHigh(48, 57))
+  .concat(arrayFromLowToHigh(33, 47))
+  .concat(arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHigh(123, 126));
+const savedPsw = document.querySelector(".saved-psw");
 
-document.addEventListener("DOMContentLoaded", () => {
-  // node elements
-  const xterAmtRange = document.getElementById("range");
-  const xterAmtNumber = document.getElementById("number");
-  const generateBtn = document.getElementById("generate");
-  const form = document.getElementById("form");
-  const displayPassword = document.querySelector(".password-display");
-  const asciiXters = arrayFromLowToHigh(97, 122)
-    .concat(arrayFromLowToHigh(48, 57))
-    .concat(arrayFromLowToHigh(33, 47))
-    .concat(arrayFromLowToHigh(58, 64))
-    .concat(arrayFromLowToHigh(91, 96))
-    .concat(arrayFromLowToHigh(123, 126));
-  const savedPsw = document.querySelector(".saved-psw");
+// event listeners
+xterAmtNumber.addEventListener("input", syncSlider);
+xterAmtRange.addEventListener("input", syncSlider);
 
-  // event listeners
-  xterAmtNumber.addEventListener("input", syncSlider);
-  xterAmtRange.addEventListener("input", syncSlider);
-
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const xterAmt = xterAmtNumber.value;
-    const password = generatePass(xterAmt);
-    displayPassword.innerText = password;
-    savePsw(password);
-    const lists = document.querySelectorAll(".psw");
-   
-      // lists.forEach((list) => {
-      //   list.children[1].addEventListener("click", (e) => {
-      //     // on each of the passwords, when the delete button gets clicked , we remove the entired div
-      //     const target = e.target;
-      //     const targetParent = target.parentElement; // target the parent elt to suppress the whole div
-      //     targetParent.remove();
-      //   });
-      // });
-    
-      lists.forEach((list) => {
-        list.children[0].addEventListener("click", (e) => {
-          const target = e.target.parentElement.children[2]
-          let copyText = document.createElement('input')
-          copyText.value = target.innerText
-          copyText.select()
-          copyText.setSelectionRange(0,99999)
-          document.execCommand("copy")
-          alert("Copied the text" + copyText.value)
-        });
-      });
-    
-  });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const xterAmt = xterAmtNumber.value;
+  const password = generatePass(xterAmt);
+  displayPassword.innerText = password;
+  savePsw(password);
+  const lists = document.querySelectorAll(".psw");
+  savedPsw.addEventListener("click", copyDelete)
+})
+  
 
   // functions
   function syncSlider(e) {
@@ -93,4 +71,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     return array;
   }
-});
+
+  function copyDelete(e){
+    const item = e.target
+    if (item.classList[0] === "trash-btn") {
+      const password= item.parentElement;
+     password.remove()
+    }
+    // copy to clipcoard
+    if (item.classList[0] === "copy-btn") {
+      const todo = item.parentElement;
+    }
+  }
